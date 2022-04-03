8b3d8cf67e4e2f55ff351a481171364b8ab8fa13
ArvinZJC <tomzjc@qq.com>
2022-04-03 08:59:12 +0800

    feat: adjust the download logic for Source 1

--- src/downloader.py
+++ src/downloader.py
@@ -1,11 +1,11 @@
 """
 '''
 Description: a Weibo Emoji image downloader
-Version: 1.0.0.20220401
+Version: 1.0.3.20220403
 Author: Arvin Zhao
 Date: 2022-03-25 15:08:27
 Last Editors: Arvin Zhao
-LastEditTime: 2022-04-01 20:35:17
+LastEditTime: 2022-04-03 08:37:02
 '''
 """
 
@@ -13,24 +13,27 @@ from typing import Dict, List, Optional
 from urllib.request import urlretrieve
 import json
 import os
+import shutil
+
+ERROR_MESSAGE_TEMPLATE = "Error: {}"
 
 
-def download(ignore_existing: bool = True):
-    """Download the Weibo Emoji images from the URLs recorded in the Weibo Emoji list.
-
-    Parameters
-    ----------
-    ignore_existing : bool
-        A flag indicating if the existing image should be redownloaded.
-    """
+def download() -> None:
+    """Download the Weibo Emoji images from the URLs recorded in the Weibo Emoji list."""
     print("Downloading Weibo Emoji images...")
     images = load_emoji_list()
+    source_1_dir = os.path.join("..", "img", "source_1")
+
+    if os.path.isdir(source_1_dir):
+        try:
+            shutil.rmtree(source_1_dir)
+        except OSError as e:
+            print(ERROR_MESSAGE_TEMPLATE.format(e))
+            return
 
     for image in images:
         category: str = image.get("category", "未定义")
-        category_dir = os.path.join(
-            "..", "img", "source_1", category if category != "" else "默认"
-        )
+        category_dir = os.path.join(source_1_dir, category if category != "" else "默认")
         url: Optional[str] = image.get("url")
         value: Optional[str] = image.get("value")
 
@@ -38,17 +41,15 @@ def download(ignore_existing: bool = True):
             print("No URL or value found in the image dict {}.".format(image))
             continue
 
-        filename = os.path.join(
-            category_dir, "{}{}".format(value[1:-1], url[url.rindex(".") :])
-        )
-
-        if os.path.isdir(category_dir):
-            if ignore_existing and os.path.isfile(filename):
-                continue
-        else:
+        if not os.path.isdir(category_dir):
             os.makedirs(category_dir)
 
-        urlretrieve(url, filename)
+        urlretrieve(
+            url,
+            os.path.join(
+                category_dir, "{}{}".format(value[1:-1], url[url.rindex(".") :])
+            ),
+        )
 
 
 def load_emoji_list() -> List[Dict]:
